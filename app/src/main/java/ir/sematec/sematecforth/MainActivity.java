e22c67eb13ebf42b152c5c97cdc1a33771a92158
y-arbabi <y.arbabi@yahoo.com>
2019-02-10 09:40:23 +0330

    Botton-Create

---
+++ app/src/main/java/ir/sematec/sematecforth/MainActivity.java
@@ -0,0 +1,18 @@
+package ir.sematec.sematecforth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnok);
+        btn.setText("NOT OK");
+    }
+}
